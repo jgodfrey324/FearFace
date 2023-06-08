2c384dd18b2039f5df501697f109928edfecbb51
tienhoang <xuantien93@yahoo.com>
2023-06-08 13:53:34 -0400

    asdsad

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from "react-redux";
-// import { useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { logout } from "../../store/session";
 // import OpenModalButton from "../OpenModalButton";
 // import LoginFormModal from "../LoginFormModal";
@@ -8,6 +8,7 @@ import { logout } from "../../store/session";
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const history = useHistory()
 
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
@@ -49,6 +50,7 @@ function ProfileButton({ user }) {
           <div className="user-menu-content">
             <span style={{ color: "whitesmoke" }}>{user.username}</span>
             <span style={{ color: "whitesmoke" }}>{user.email}</span>
+            <span onClick={() => history.push(`/users/${user.id}`)} style={{ color: 'whitesmoke' }}>My Profile</span>
             <button className="logout-button" style={{ color: "whitesmoke" }} onClick={handleLogout}>Log Out</button>
 
           </div>
