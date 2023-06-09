730bad0a78d7cc5d5f8b47881b84184436647340
Raoul Andalis <raoulandalis1@yahoo.com>
2023-06-09 03:08:34 -0400

    damn

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, EmailField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
@@ -23,7 +23,7 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
+    email = EmailField('email', validators=[DataRequired(), user_exists])
     password = StringField('password', validators=[DataRequired()])
     first_name = StringField('first_name',validators=[DataRequired()])
     last_name = StringField("last_name",validators=[DataRequired()])
--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -113,7 +113,7 @@ export const signUp = (username, email, password, first_name, last_name) => asyn
 	if (response.ok) {
 		const data = await response.json();
 		dispatch(setUser(data));
-		return null;
+		return data;
 	} else if (response.status < 500) {
 		const data = await response.json();
 		if (data.errors) {
